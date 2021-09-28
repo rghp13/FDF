21d7d9d93c4eb0946fe7cc38a10d3631dc717143
Romain <rghp13@gmail.com>
2021-09-28 19:31:05 +0200

    additional install in setup_linux

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,4 +1,4 @@
-sudo apt-get update && sudo apt-get install xorg libxext-dev zlib1g-dev
+sudo apt-get update && sudo apt-get install xorg libxext-dev zlib1g-dev libbsd-dev
 cd minilibx-linux
 git submodule init
 git submodule update
